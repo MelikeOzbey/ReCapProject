408fa6fef8a96e3e0204c1c560043ef1ce71c095
Melike <melikenurozbey@outlook.com>
2021-02-01 22:48:57 +0300

    Modified

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -14,19 +14,40 @@ namespace ConsoleUI
         {
             CarManager carManager = new CarManager(new InMemoryManager());
             Car car = new Car { Id = 6, BrandId = 7, ColorId = 1, ModelYear = new DateTime(2020, 01, 01), DailyPrice = 200, Description = "Hyundai otomatik vites" };
-
+            
+            Console.WriteLine("----------------Add/GetById(idsi 5 olan)----------------------");
             carManager.Add(car);
-            Car c=carManager.GetById(5);
-            Console.WriteLine(c.Description);
+            Car carTest1=carManager.GetById(5);
+            Console.WriteLine(carTest1.Description);
 
-            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("-------------------GetAll-----------------------");
 
             List<Car> cars = carManager.GelAll();
             foreach (var item in cars)
             {
                 Console.WriteLine(item.Description);
             }
-            
+
+            Console.WriteLine("--------------------Update(idsi 5 olan)-------------------------");
+
+            Car carTest2 = new Car { Id = 5, BrandId = 8, ColorId = 1, ModelYear = new DateTime(2020, 01, 01), DailyPrice = 200, Description = "Ford otomatik vites" };
+            carManager.Update(carTest2);
+
+            List<Car> cars2 = carManager.GelAll();
+            foreach (var item in cars2)
+            {
+                Console.WriteLine(item.Description);
+            }
+
+            Console.WriteLine("--------------------Delete(idsi 5 olan)-------------------------");
+
+            carManager.Delete(carTest2);
+            List<Car> cars3 = carManager.GelAll();
+            foreach (var item in cars3)
+            {
+                Console.WriteLine(item.Description);
+            }
         }
+
     }
 }
