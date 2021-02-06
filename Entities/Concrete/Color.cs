ce73bde012e296ec0a4156afafd0370cbe36a880
Melike <melikenurozbey@outlook.com>
2021-02-06 20:21:06 +0300

    Modified

--- Entities/Concrete/Color.cs
+++ Entities/Concrete/Color.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Entities.Concrete
 {
-    public class Color
+    public class Color:IEntity
     {
         public int Id { get; set; }
         public string ColorName { get; set; }
